dc7e68ba608b3011c4581e0fe57dc6d972003a64
HAMDI <hamdihassine3@gmail.com>
2017-11-20 22:31:01 +0100

    calculate The utility matrix

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 from database import *
+import numpy as np
+
+
 
 conn=connection()
-req="""SELECT user_id, item_id, rating FROM rating_base"""
-rows=execute_req(req)
 
 #test database
+"""
 for row in rows:
 	print('{0} : {1} : {2}'.format(row[0], row[1], row[2]))
 	
 close_connection(conn)	
+"""
+
+req_user="""SELECT * FROM user"""
+user=execute_req(req_user)
+
+req_item="""SELECT * FROM item"""
+item=execute_req(req_item)
+
+req_rating_base="""SELECT * FROM rating_base"""
+rating=execute_req(req_rating_base)
+
+req_rating_test="""SELECT * FROM rating_test"""
+rating_test=execute_req(req_rating_test)
+
+
+n_users = len(user)
+n_items = len(item)
+
+
+# The utility matrix is used to store the rating for each user-item pair in the
+# matrix form
+
+utility = np.zeros((n_users, n_items))
+for r in rating:
+	var_user_id=0
+	var_item_id=1
+	var_rating=2
+	var_timestamp=3
+	utility[r[var_user_id] - 1][r[var_item_id] - 1] = r[var_rating]
+ 
+
+   
+print(utility)
