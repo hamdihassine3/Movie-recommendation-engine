957718fe352735e46dfd8d0773f53dd85259a06e
HAMDI <hamdihassine3@gmail.com>
2017-11-26 16:43:37 +0100

    calculate pairwase distance + prediction + evaluation

---
+++ CollaborativeFiltering.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+
+# define the predict function 
+def predict(ratings, similarity, type='user'):
+    if type == 'user':
+        mean_user_rating = ratings.mean(axis=1)
+        #You use np.newaxis so that mean_user_rating has same format as ratings
+        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+    elif type == 'item':
+        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+    return pred
+
+
+#Evaluation
+def rmse(prediction, ground_truth):
+    prediction = prediction[ground_truth.nonzero()].flatten()
+    ground_truth = ground_truth[ground_truth.nonzero()].flatten()
+    return sqrt(mean_squared_error(prediction, ground_truth))
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from database import *
+from CollaborativeFiltering import *
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import pairwise_distances
+from scipy.spatial.distance import pdist, squareform
 
 
 conn=connection()
@@ -42,14 +44,31 @@ for r in rating:
 	base_timestamp=3
 	utility[r[base_user_id] - 1][r[base_item_id] - 1] = r[base_rating]
 
+#we will calculate in each loop Pairwise distances of train_data, calculate user_prediction ,
+# calculate the evaluation and comare the result with the other evaluations 
+L=['braycurtis', 'canberra', 'chebyshev', 'cityblock', 'correlation', 'cosine', 'euclidean', 'hamming','minkowski','sqeuclidean']
+best_rmse=10
+best_user_prediction={}
+for i in range(10):	
+	# Create two user-item matrices , one for training and another for testing
+	train_data, test_data = train_test_split(utility, test_size=0.25)
+	#calculate similarity
+	user_similarity = squareform(pdist(train_data, L[i]))
+	#calculate preduction
+	#item_prediction = predict(train_data, item_similarity, type='item')
+	user_prediction = predict(train_data, user_similarity, type='user')
+	#calcul evaluation
+	value_rmse=rmse(user_prediction, test_data)
+	if(value_rmse<best_rmse):
+		best_rmse=value_rmse
+		best_user_prediction=user_prediction
 
-# Create two user-item matrices , one for training and another for testing
-train_data, test_data = train_test_split(utility, test_size=0.25)
 
-#calculate the cosine similarity
-user_similarity = pairwise_distances(train_data, metric='cosine')
-item_similarity = pairwise_distances(train_data.T, metric='cosine')
+print(best_user_prediction)
+print 'User-based CF RMSE: ' + str(best_rmse) 
 
+#print 'User-based CF RMSE: ' + str(rmse(user_prediction, test_data)) 
+#print 'Item-based CF RMSE: ' + str(rmse(item_prediction, test_data))
 
 
 #on peut utiliser cette partie a la fin pour faire plus de test en utilisant la table rating_test de notre base 
